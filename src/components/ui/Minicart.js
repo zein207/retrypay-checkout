a8bb6f908620f353e66be28596967e38a63f1ac7
David Ponce <zein207@gmail.com>
2023-02-27 23:38:23 -0600

    Minicart styles

--- src/components/ui/Minicart.js
+++ src/components/ui/Minicart.js
@@ -4,35 +4,35 @@ export const Minicart = () => {
   return (
     <div className='minicart-summary-container'>
         <div className='minicart-summary__copy'>
-            <span>Producto</span>
-            <span>Monto</span>
+            <span className='w-75'>Producto</span>
+            <span className='w-25'>Monto</span>
         </div>
         <div className='minicart-summary__products'>
             <div className='minicart-product__item'>
-                <span className='minicart-product__name'>Facial Cleanser 125ml</span>
-                <span className='minicart-product__price'>$546.90</span>
+                <span className='minicart-product__name w-75'>Facial Cleanser 125ml</span>
+                <span className='minicart-product__price w-25'>$546.90</span>
             </div>
             <div className='minicart-product__item'>
-                <span className='minicart-product__name'>Facial Cleanser 125ml</span>
-                <span className='minicart-product__price'>$546.90</span>
+                <span className='minicart-product__name w-75'>Facial Cleanser 125ml</span>
+                <span className='minicart-product__price w-25'>$546.90</span>
             </div>
             <div className='minicart-product__item'>
-                <span className='minicart-product__name'>Facial Cleanser 125ml</span>
-                <span className='minicart-product__price'>$546.90</span>
+                <span className='minicart-product__name w-75'>Facial Cleanser 125ml</span>
+                <span className='minicart-product__price w-25'>$546.90</span>
             </div>
         </div>
         <div className='minicart-summary__price-info'>
-            <div className='minicart-summary__subtotal'>
-                <span>Sub - Total amount</span>
-                <span className='minicart-summary__subtotal-price'>$1,638.00</span>
+            <div className='minicart-summary__subtotal flex'>
+                <span className='w-75'>Sub - Total amount</span>
+                <span className='minicart-summary__subtotal-price w-25'>$1,638.00</span>
             </div>
-            <div className='minicart-summary__taxes'>
-                <span>Impuestos</span>
-                <span className='minicart-summary__subtotal-price'>$0.00</span>
+            <div className='minicart-summary__taxes flex'>
+                <span className='w-75'>Impuestos</span>
+                <span className='minicart-summary__subtotal-price w-25'>$0.00</span>
             </div>
-            <div className='minicart-summary__total'>
-                <span>Total</span>
-                <span className='minicart-summary__total-price'>$1,638.00</span>
+            <div className='minicart-summary__total flex'>
+                <span className='w-75'>Total</span>
+                <span className='minicart-summary__total-price w-25'>$1,638.00</span>
             </div>
         </div>
     </div>
